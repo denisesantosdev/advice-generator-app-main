45a0e37b6add93096c9226b37c31161b461105b3
denisesantosdev <denisesantosdev@gmail.com>
2023-03-31 16:25:46 -0300

    javascript done

---
+++ script.js
@@ -0,0 +1,24 @@
+const updateAdviceBtn = document.querySelector(".dice-btn");
+const adviceIdNumber = document.querySelector("#advice-id-number");
+const adviceText = document.querySelector(".advice-text");
+
+updateAdviceBtn.addEventListener('click', getAdviceData)
+
+async function getAdviceData() {
+  const url = `https://api.adviceslip.com/advice`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  displayAdvice(data);
+}
+
+getAdviceData();
+
+function displayAdvice(data) {
+  const advice = data.slip.advice;
+  const adviceId = data.slip.id;
+
+  adviceIdNumber.innerHTML = adviceId
+  adviceText.innerHTML = advice
+}
