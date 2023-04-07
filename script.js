21d31c93550ee833d8434039df956300938134a5
denisesantosdev <denisesantosdev@gmail.com>
2023-04-07 19:57:51 -0300

    adicionado comparação entre async/wait e .then()

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ const updateAdviceBtn = document.querySelector(".dice-btn");
 const adviceIdNumber = document.querySelector("#advice-id-number");
 const adviceText = document.querySelector(".advice-text");
 
-updateAdviceBtn.addEventListener('click', getAdviceData)
+updateAdviceBtn.addEventListener("click", getAdviceData);
 
-async function getAdviceData() {
+/* async function getAdviceData() {
   const url = `https://api.adviceslip.com/advice`;
 
   const response = await fetch(url);
   const data = await response.json();
 
-  displayAdvice(data);
+  displayAdvice(data); 
+} */
+
+// o mesmo que
+function getAdviceData() {
+  const url = `https://api.adviceslip.com/advice`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      displayAdvice(data);
+    });
 }
 
-getAdviceData()
+getAdviceData();
 
 function displayAdvice(data) {
   const advice = data.slip.advice;
   const adviceId = data.slip.id;
 
-  adviceIdNumber.innerHTML = adviceId
-  adviceText.innerHTML = `"${advice}"`
+  adviceIdNumber.innerHTML = adviceId;
+  adviceText.innerHTML = `"${advice}"`;
 }
